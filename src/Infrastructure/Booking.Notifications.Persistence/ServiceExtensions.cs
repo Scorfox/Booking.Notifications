ada3ab814fa181fe6ba7051f53da10486bd36148
Haxend <dunaevskyilya@gmail.com>
2024-02-14 02:29:39 +0300

    Fix

---
+++ src/Infrastructure/Booking.Notifications.Persistence/ServiceExtensions.cs
@@ -0,0 +1,15 @@
+﻿using Booking.Notifications.Persistence.Context;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Booking.Notifications.Persistence;
+
+public static class ServiceExtensions
+{
+    public static void ConfigurePersistence(this IServiceCollection services, IConfiguration configuration)
+    {
+        var connectionString = configuration.GetConnectionString("PostgreSQL");
+        
+        services.AddDbContext<DataContext>(opt => { opt.UseNpgsql(connectionString); });    }
+}
